39ac432b87c5093ea1bf1426084417ab7b67e2d3
Ashley Marku <ashley.marku@lc.cuny.edu>
2018-01-09 16:40:25 -0500

    first upload

---
+++ argue.js
@@ -0,0 +1,9 @@
+function changeRealAnswer() {
+
+	document.getElementById("one").innerHTML = "Im right!";
+}
+
+function changeAnswer() {
+
+	document.getElementById("one").innerHTML = "No, im right!";
+}
